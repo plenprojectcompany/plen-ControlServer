2cd85d87439646b56eab2b936ecca4bc5364ac1d
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2017-01-19 17:20:27 +0900

    Change the application starting up method

--- control_server_gui/control_server_gui/angularjs/index.ts
+++ control_server_gui/control_server_gui/angularjs/index.ts
@@ -1,4 +1,12 @@
 ﻿"use strict";
 
-var APP_NAME = "PLEN Utils";
+// The application module's namespace definition.
+var APP_NAME: string = "PLEN Utils";
+
 angular.module(APP_NAME, []);
+
+// @attention If you use Batarang, you should disable "strictDi" property.
+angular.element(() =>
+{
+    angular.bootstrap(document.body, [APP_NAME], { strictDi: true });
+});
